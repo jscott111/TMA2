5d3b69edd7d7e59021fe7e1af68ec0cbf38d3ffe
jscott111 <33634599+jscott111@users.noreply.github.com>
2021-10-18 12:42:53 -0300

    Update start.php

--- TMA2/part2/start.php
+++ TMA2/part2/start.php
@@ -8,13 +8,18 @@
      <body style="font-family:Arial; margin: 0;">
          <?php
 
-             $connectionInfo = array("UID" => "jscott11", "pwd" => "3557321Joh--", "Database" => "lms", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-             $serverName = "tcp:jscott11.database.windows.net,1433";
-             $database = sqlsrv_connect($serverName, $connectionInfo);
+             try {
+                 $conn = new PDO("sqlsrv:server = tcp:jscott11.database.windows.net,1433; Database = lms", "jscott11", "3557321Joh--");
+                 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+             }
+             catch (PDOException $e) {
+                 print("Error connecting to SQL Server.");
+                 die(print_r($e));
+             }
       
              $xml = simplexml_load_file("material.xml");
 
-             $getResults = sqlsrv_query($database, "DELETE FROM [dbo].[material]");
+             $conn->query("DELETE FROM [dbo].[material]");
              if ($getResults == FALSE){
                 echo (sqlsrv_errors());
              }
@@ -22,7 +27,7 @@
              foreach($xml->course as $course){
                  foreach($course->unit as $unit){
                      foreach($unit->subUnit as $subUnit){
-                        sqlsrv_query($database, "INSERT INTO [dbo].[material] (code, unit, subunit, courseContent, courseName) VALUES (" . $course->code . " , " . $unit->title . ", " . $subUnit->number . ", '" . $subUnit->content . "', '" . $course->name . "')");
+                        $conn->query("INSERT INTO [dbo].[material] (code, unit, subunit, courseContent, courseName) VALUES (" . $course->code . " , " . $unit->title . ", " . $subUnit->number . ", '" . $subUnit->content . "', '" . $course->name . "')");
                      }
                  }
              }
@@ -32,7 +37,7 @@
       
              $xml = simplexml_load_file("questions.xml");
 
-             sqlsrv_query($database, "DELETE FROM dbo.questions");
+             $conn->query("DELETE FROM dbo.questions");
 
              foreach($xml->course as $course){
                 $num = $course->number;
@@ -40,7 +45,7 @@
                 foreach($course->question as $question){
                     $text = $question->text;
                     $query = "INSERT INTO dbo.questions (course, question, answera, answerb, answerc, answerd, correct, courseName) VALUES (" . $num . " , '" . $text . "', '" . $question->answera->letter . ": " . $question->answera->text . "', '" . $question->answerb->letter . ": " . $question->answerb->text . "', '" . $question->answerc->letter . ": " . $question->answerc->text . "', '" . $question->answerd->letter . ": " . $question->answerd->text . "', '" . $question->correct . "', '" . $name . "')";
-                    sqlsrv_query($database, $query);
+                    $conn->query($query);
                 }
              }
          ?>
