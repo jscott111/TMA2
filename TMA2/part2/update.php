0ce236956d73d33e7024ff8f350c9ed57bc92f3b
jscott111 <33634599+jscott111@users.noreply.github.com>
2021-10-18 15:14:33 -0300

    Update update.php

--- TMA2/part2/update.php
+++ TMA2/part2/update.php
@@ -8,21 +8,26 @@
             $pass = $_POST['password'];
             $course = $_POST['course'];
             $grade = $_POST['grade'];
-            if (!($database = new mysqli("127.0.0.1:3306", "root", "3557321Joh--", "lms"))){
-                echo "Failed to connect";
-            }
+            try {
+                 $conn = new PDO("sqlsrv:server = tcp:jscott11.database.windows.net,1433; Database = lms", "jscott11", "3557321Joh--");
+                 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+             }
+             catch (PDOException $e) {
+                 print("Error connecting to SQL Server.");
+                 die(print_r($e));
+             }
 
-            $result = $database->query("SELECT grade FROM lms.grades WHERE user='" . $user . "' AND course=" . $course);
+            $result = $database->query("SELECT grade FROM [lms].[grades] WHERE user='" . $user . "' AND course=" . $course);
             foreach($result as $results){
                 $DBGrade = $results['grade'];
             }
 
             if($grade > $DBGrade){
                 if($result->num_rows == 0){
-                    $database->query("INSERT INTO lms.grades (user, course, grade) VALUES ('" . $user . "', " . $course . ", " . $grade . ")");
+                    $database->query("INSERT INTO [lms].[grades] (user, course, grade) VALUES ('" . $user . "', " . $course . ", " . $grade . ")");
                 }
                 else{
-                    $database->query("UPDATE lms.grades SET grade=" . $grade . " WHERE user='" . $user . "' AND course=" . $course);
+                    $database->query("UPDATE [lms].[grades] SET grade=" . $grade . " WHERE user='" . $user . "' AND course=" . $course);
                 }
             }
             echo "<form id='homeForm' action='home.php' method='POST'>";
