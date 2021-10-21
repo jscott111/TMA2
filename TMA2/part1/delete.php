a25326722b58f889cb59bfb5f1ef66097da6464c
jscott111 <33634599+jscott111@users.noreply.github.com>
2021-10-21 15:21:16 -0300

    Update delete.php

--- TMA2/part1/delete.php
+++ TMA2/part1/delete.php
@@ -4,11 +4,16 @@
     </head>
     <body>
         <?php
-            if (!($database = new mysqli("127.0.0.1:3306", "root", "3557321Joh--", "bookmarks"))){
-                echo "Failed to connect";
+            try {
+                $conn = new PDO("sqlsrv:server = tcp:jscott11.database.windows.net,1433; Database = bookmarks", "jscott11", "3557321Joh--");
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch (PDOException $e) {
+                print("Error connecting to SQL Server.");
+                die(print_r($e));
             }
 
-            $database->query("DELETE FROM bookmarks.marks WHERE id = '" . $_POST['userid'] . "' AND link = '" . $_POST['link'] . "';");
+            $conn->query("DELETE FROM [dbo].[marks] WHERE id = '" . $_POST['userid'] . "' AND link = '" . $_POST['link'] . "';");
 
             echo "<form id='form' action='home.php' method='POST'>";
             echo "<input id='username' name='username' type='hidden' value='" . $_POST['username'] . "'></input>";
