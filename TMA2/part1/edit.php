916b995aa91c3cf6cfe8703c3e2efe0906512907
jscott111 <33634599+jscott111@users.noreply.github.com>
2021-10-21 15:20:20 -0300

    Update edit.php

--- TMA2/part1/edit.php
+++ TMA2/part1/edit.php
@@ -4,11 +4,16 @@
     </head>
     <body>
         <?php
-            if (!($database = new mysqli("127.0.0.1:3306", "root", "3557321Joh--", "bookmarks"))){
-                echo "Failed to connect";
+            try {
+                $conn = new PDO("sqlsrv:server = tcp:jscott11.database.windows.net,1433; Database = bookmarks", "jscott11", "3557321Joh--");
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch (PDOException $e) {
+                print("Error connecting to SQL Server.");
+                die(print_r($e));
             }
 
-            $database->query("UPDATE bookmarks.marks SET link = '" . $_POST['editLink'] . "', id = '" . $_POST['userid'] . "' WHERE link = '" . $_POST['link'] . "';");
+            $conn->query("UPDATE [dbo].[marks] SET link = '" . $_POST['editLink'] . "', id = '" . $_POST['userid'] . "' WHERE link = '" . $_POST['link'] . "';");
 
             echo "<form id='form' action='home.php' method='POST'>";
             echo "<input id='link' name='link' type='hidden' value='" . $_POST['editLink'] . "'></input>";
